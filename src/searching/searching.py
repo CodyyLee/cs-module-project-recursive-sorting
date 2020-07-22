9981357043eaf3c3c135465affcce0b6ef8e0c6b
Cody Garrett <cody.l.garrett22@gmail.com>
2020-07-22 12:03:43 -0600

    unable to complete

--- src/searching/searching.py
+++ src/searching/searching.py
@@ -1,6 +1,27 @@
 # TO-DO: Implement a recursive implementation of binary search
 def binary_search(arr, target, start, end):
     # Your code here
+    if len(arr) == 0:
+        return -1
+    elif len(arr) == 1:
+        if arr[0] == target:
+            return 0
+        else:
+            return -1
+    else:
+        middle = ((end - start) + 1) // 2
+
+        if arr[middle] == target:
+            return middle
+        else:
+            if arr[middle] < target:
+                return binary_search(arr, target, middle, end)
+            else:
+                return binary_search(arr, target, start, middle)
+
+
+arr1 = [-9, -8, -6, -4, -3, -2, 0, 1, 2, 3, 5, 7, 8, 9]
+print(binary_search(arr1, 0, 0, len(arr1) - 1))
 
 
 # STRETCH: implement an order-agnostic binary search
@@ -9,6 +30,6 @@ def binary_search(arr, target, start, end):
 # sorted in ascending order or in descending order
 # You can implement this function either recursively 
 # or iteratively
-def agnostic_binary_search(arr, target):
+# def agnostic_binary_search(arr, target):
     # Your code here
 
--- src/sorting/sorting.py
+++ src/sorting/sorting.py
@@ -2,27 +2,65 @@
 def merge(arrA, arrB):
     elements = len(arrA) + len(arrB)
     merged_arr = [0] * elements
+    x = 0
 
     # Your code here
-
-
+    for i in range(0, len(merged_arr)):
+        if len(arrA) + len(arrB) == 0:
+            return
+        if len(arrA) + len(arrB) == 1:
+            if len(arrA) != 0:
+                merged_arr[i] = arrA[0]
+                arrA.pop(0)
+            else:
+                merged_arr[i] = arrB[0]
+                arrB.pop(0)
+        elif len(arrA) == 0 and len(arrB) != 0:
+            if arrB[0] < arrB[1]:
+               merged_arr[i] = arrB[0]
+               arrB.pop(0)
+            else:
+                merged_arr[i] = arrB[1]
+                arrB.pop(1)
+        elif len(arrB) == 0 and len(arrA) != 0:
+            if arrA[0] < arrA[1]:
+               merged_arr[i] = arrA[0]
+               arrA.pop(0)
+            else:
+                merged_arr[i] = arrB[1]
+                arrB.pop(1)
+        elif arrA[0] < arrB[0]:
+            merged_arr[i] = arrA[0]
+            arrA.pop(0)
+        else:
+            merged_arr[i] = arrB[0]
+            arrB.pop(0)
     return merged_arr
 
 # TO-DO: implement the Merge Sort function below recursively
 def merge_sort(arr):
-    # Your code here
+#     # Your code here
+    if len(arr) <= 1:
+        return arr
+    else:
+        middle = len(arr) // 2
+        left = merge_sort(arr[:middle])
+        right = merge_sort(arr[middle:])
+        
+    return merge(left, right)
 
-
-    return arr
+# arr = [1, 5, 8, 4, 2, 9, 6, 0, 3, 7]
+# print(arr[:5])
+# print(arr[5:])
 
 # STRETCH: implement the recursive logic for merge sort in a way that doesn't 
 # utilize any extra memory
 # In other words, your implementation should not allocate any additional lists 
 # or data structures; it can only re-use the memory it was given as input
-def merge_in_place(arr, start, mid, end):
-    # Your code here
+# def merge_in_place(arr, start, mid, end):
+#     # Your code here
 
 
-def merge_sort_in_place(arr, l, r):
+# def merge_sort_in_place(arr, l, r):
     # Your code here
 
